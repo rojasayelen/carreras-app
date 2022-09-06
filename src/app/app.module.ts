7dbaa4dfbb2461e48dc168f4688fbd4df5ddbd62
rojasayelen <aye.estudio@gmail.com>
2022-09-06 20:18:14 -0300

    subida inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { HomeComponent } from './home/home.component';
+import { PilotosComponent } from './pilotos/pilotos.component';
+
 import { MenuComponent } from './menu/menu.component';
+import { CarrerasComponent } from './carreras/carreras.component';
+import { AutodromosComponent } from './autodromos/autodromos.component';
+
+
+const router: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'pilotos',
+    component: PilotosComponent
+  },
+  {
+    path: 'autodromos',
+    component: AutodromosComponent
+  },
+  {
+    path: 'carreras',
+    component: CarrerasComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     MenuComponent
-  ],
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatSliderModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(router)
   ],
   providers: [],
   bootstrap: [AppComponent]
